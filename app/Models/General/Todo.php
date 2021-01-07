0aae6d42c75434417fbde636fb38c36ee737decc
Chijioke <okaforchijioke01@gmail.com>
2021-01-07 11:57:39 +0100

    added color to model and migration

--- app/Models/General/Todo.php
+++ app/Models/General/Todo.php
@@ -3,6 +3,7 @@
 namespace App\Models\General;
 
 use App\Helpers\Uuids;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -14,5 +15,20 @@ class Todo extends Model
     public $incrementing = false;
     protected $keyType = 'string';
 
-    protected $fillable = ['type', 'name', 'description', 'pop', 'mail'];
+    protected $fillable = [
+        'user_id',
+        'ip_address',
+        'type',
+        'name',
+        'description',
+        'date',
+        'color',
+        'pop',
+        'mail',
+        'completed'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Helpers\Uuids;
+use App\Models\General\Todo;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -44,4 +45,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function todos() {
+        return $this->hasMany(Todo::class);
+    }
 }
